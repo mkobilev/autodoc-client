2ee9619c02523aec4f546e1b6ba24a1996578087
Oleg Khabarov <khabar.oleg@gmail.com>
2018-06-07 11:27:22 +0300

    Update claim-details component to get and render data from backend

--- src/app/pages/claim-details/claim-details.component.ts
+++ src/app/pages/claim-details/claim-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ClaimService } from '../../core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-claim-details',
@@ -6,10 +8,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./claim-details.component.css']
 })
 export class ClaimDetailsComponent implements OnInit {
+  claimId: String = '';
 
-  constructor() { }
+  claimDetails: ClaimDetails;
+
+  constructor(
+    private claimService: ClaimService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.route.url.subscribe(data => this.claimId = data[data.length - 1].path);
+    this.claimService.getClaimDetail(this.claimId).subscribe(
+      data => {
+        this.claimDetails = {
+          id: data._id.$oid,
+          created_at: data.created_at.$date,
+          request_person_id: data.request_person_id.$oid,
+          status: data.status,
+          dst_city: data.claim_data.dst_city,
+          dst_country: data.claim_data.dst_country,
+          dst_organization: data.claim_data.dst_organization,
+          goal: data.claim_data.goal,
+          start_date: data.claim_data.start_date.$date,
+          finish_date: data.claim_data.finish_date.$date,
+          term: data.claim_data.term,
+          financial_source: data.claim_data.financial_source,
+          transport: data.claim_data.transport,
+          head_of_department: data.claim_data.head_of_department
+        }
+      }
+    )
   }
 
+}
+
+interface ClaimDetails {
+  id: string;
+  created_at: number;
+  request_person_id: string;
+  status: string;
+  dst_city: string;
+  dst_country: string;
+  dst_organization: string;
+  goal: string;
+  start_date: number;
+  finish_date: number;
+  term: number;
+  financial_source: string;
+  transport: string;
+  head_of_department: string;
 }
